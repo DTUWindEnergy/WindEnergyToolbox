dbc94a38b9707c3874b3d11011a9405280b20565
madsmpedersen <m@madsp.dk>
2016-01-28 12:01:27 +0100

    readlines -> utf-8

--- wetb/hawc2/htc_file.py
+++ wetb/hawc2/htc_file.py
@@ -51,7 +51,7 @@ class HTCFile(HTCContents, HTCDefaults):
     def readlines(self, filename):
         self.htc_inputfiles.append(filename)
         htc_lines = []
-        with open(filename) as fid:
+        with open(filename, encoding='utf-8') as fid:
             lines = fid.readlines()
         for l in lines:
             if l.lower().lstrip().startswith('continue_in_file'):
