4ec29c2bd52e4d9ec67414e64d338f5015e84e6e
madsmpedersen <m@madsp.dk>
2016-04-07 11:07:46 +0200

    integrated changes from gorm

--- wetb/hawc2/cluster_simulation.py
+++ wetb/hawc2/cluster_simulation.py
@@ -4,15 +4,15 @@ import sys
 from threading import  Thread
 class ClusterSimulation(Simulation):
     def __init__(self, modelpath, htcfilename, hawc2exe="HAWC2MB.exe"):
-        print ("start")
         Simulation.__init__(self, modelpath, htcfilename, hawc2exe=hawc2exe)
+
         self.simulation_id = [f for f in os.listdir('.') if f.endswith('.in')][0][:-3]
-        self.resource.simulationThread.low_priority = False
+        self.host.simulationThread.low_priority = False
         self.non_blocking_simulation_thread = Thread(target=self.simulate)
         self.start(1)
         self.wait()
-        print (self.resource.simulationThread.res[1])  # print hawc2 output to stdout
-        sys.exit(self.resource.simulationThread.res[0])
+        print (self.host.simulationThread.res[1])  # print hawc2 output to stdout
+        sys.exit(self.host.simulationThread.res[0])
 
     def update_status(self, *args, **kwargs):
         Simulation.update_status(self, *args, **kwargs)
