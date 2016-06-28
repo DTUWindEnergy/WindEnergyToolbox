826ca2bcf3163d18c77d76139199b99a1e37f462
madsmpedersen <m@madsp.dk>
2016-06-28 10:07:37 +0200

    test

--- wetb/__init__.py
+++ wetb/__init__.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 from future import standard_library
 standard_library.install_aliases()
 import pkg_resources
-test = "TEST"
+test = "TEST" 
 try:
     __version__ = pkg_resources.get_distribution(__name__).version
 except:
