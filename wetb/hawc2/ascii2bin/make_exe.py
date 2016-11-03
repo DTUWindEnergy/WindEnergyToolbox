2e3401285a6badac9ce61f83d15fd23b2b9ed4e0
Mads M. Pedersen <mmpe@dtu.dk>
2016-11-03 14:09:16 +0100

    fix py.test thing

--- wetb/hawc2/ascii2bin/make_exe.py
+++ wetb/hawc2/ascii2bin/make_exe.py
@@ -6,9 +6,9 @@ Created on 06/09/2013
 from __future__ import division, print_function, absolute_import, unicode_literals
 from future import standard_library
 standard_library.install_aliases()
-from build_exe.cx.build_cx_exe import NUMPY
 
 
 if __name__=="__main__":
+    from build_exe.cx.build_cx_exe import NUMPY
     from build_exe.cx import build_cx_exe
     build_cx_exe.build_exe("ascii2bin.py", version="3.0.1", includes=["'pandas'"], modules=['email', NUMPY])
