3fe7ef5fb7d946a27b92add84301d27e6cbee0c5
Mads M. Pedersen <mmpe@dtu.dk>
2017-03-29 11:47:54 +0200

    missing files

---
+++ wetb/signal/tests/test_differentiation.py
@@ -0,0 +1,20 @@
+'''
+Created on 29. mar. 2017
+
+@author: mmpe
+'''
+import unittest
+import numpy as np
+from wetb.signal.filters._differentiation import differentiation
+
+class Test(unittest.TestCase):
+
+
+    def testDifferentiation(self):
+        np.testing.assert_array_equal(differentiation([1,2,1,0,1,1]), [1,0,-1,0,.5,0])
+        
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testDifferentiation']
+    unittest.main()
