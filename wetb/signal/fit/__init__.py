36122f04dea52907a3ee308814a34fc423ddf2b6
Mads M. Pedersen <mmpe@dtu.dk>
2017-03-31 11:07:11 +0200

    spline_fit import in module

--- wetb/signal/fit/__init__.py
+++ wetb/signal/fit/__init__.py
@@ -4,5 +4,6 @@ d = dir()
 from wetb.signal.fit._linear_fit import *
 from wetb.signal.fit._bin_fit import *
 from wetb.signal.fit._fourier_fit import *
+from wetb.signal.fit._spline_fit import *
 
 __all__ = sorted([m for m in set(dir()) - set(d)])
