11a5f71e41a29734850665ef30b36eb52dc1114d
Mads M. Pedersen <mmpe@dtu.dk>
2017-07-21 10:12:22 +0200

    updated test_files.py

--- wetb/utils/test_files.py
+++ wetb/utils/test_files.py
@@ -20,24 +20,19 @@ def get_test_file(filename):
     if os.path.exists(filename):
         return filename
     else:
-        filename2 = os.path.realpath(os.path.join(wetb_rep_path, 'downloaded_test_files', os.path.relpath(filename, wetb_rep_path)))
-        if not os.path.isfile(filename2):
-            #url = 'https://gitlab.windenergy.dtu.dk/toolbox/TestFiles/%s'%os.path.relpath(filename, wetb_rep_path)
-            url = 'http://tools.windenergy.dtu.dk/TestFiles/%s.txt'%os.path.relpath(filename, wetb_rep_path).replace("\\","/")
-            print ("download %s\nfrom %s"%(filename, url))
-            if not os.path.exists(os.path.dirname(filename2)):
-                os.makedirs(os.path.dirname(filename2))
-            urllib.request.urlretrieve(url, filename2)
-        return filename2
+        wetb_rep_path = os.path.join(os.path.dirname(wetb.__file__), "../")
+        return os.path.join(wetb_rep_path, 'TestFiles', os.path.relpath(filename, wetb_rep_path))
+        
 
 
 
 def move2test_files(filename,TestFile_rep_path=default_TestFile_rep_path):
     wetb_rep_path = os.path.join(os.path.dirname(wetb.__file__), "../")
-    folder = os.path.dirname(TestFile_rep_path + os.path.relpath(filename, wetb_rep_path))
+    dst_filename = os.path.join(wetb_rep_path, 'TestFiles', os.path.relpath(filename, wetb_rep_path))
+    folder = os.path.dirname(dst_filename)
     if not os.path.exists(folder):
         os.makedirs(folder)
-    os.rename(filename, os.path.join(folder, os.path.basename(filename)+'.txt'))
+    os.rename(filename, dst_filename)
     
     
     
--- wetb/utils/tests/test_test_files.py
+++ wetb/utils/tests/test_test_files.py
@@ -13,28 +13,23 @@ import wetb
 tfp = os.path.join(os.path.dirname(__file__) + "/test_files/")
 class Test_test_files(unittest.TestCase):
     def test_move2test_files(self):
-        dst = test_files.default_TestFile_rep_path+ "wetb/utils/tests/test_files/tmp_test_file.txt"
+        dst = os.path.dirname(wetb.__file__) + "/../TestFiles/wetb/utils/tests/test_files/tmp_test_file.txt"
         src = tfp+'tmp_test_file.txt'
-        if os.path.isdir(test_files.default_TestFile_rep_path):
-            if os.path.isfile(dst):
-                os.remove(dst)
-            if not os.path.isfile(src):
-                with open(src,'w') as fid:
-                    fid.write("This is a test file")
-            move2test_files(src)
-            self.assertTrue(os.path.isfile(dst))
+        if os.path.isfile(dst):
+            os.remove(dst)
+        if not os.path.isfile(src):
+            with open(src,'w') as fid:
+                fid.write("This is a test file")
+        move2test_files(src)
+        self.assertTrue(os.path.isfile(dst))
 
     def test_test_files(self):
-        fn = os.path.realpath(os.path.dirname(wetb.__file__) + '/../downloaded_test_files/wetb/utils/tests/test_files/test_file.txt')
-        if os.path.isfile(fn):
-            os.remove(fn)
         fn1 = get_test_file(tfp+'test_file.txt')
         self.assertTrue(fn1)
-        os.remove(fn1)
         fn2 = get_test_file('test_file.txt')
-        self.assertEqual(fn2, fn)
-        self.assertTrue(os.path.isfile(fn2))
+        self.assertEqual(fn2, fn1)
         
+         
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
