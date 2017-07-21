b6e86eab84f4627353e6bbf61c270e542ff4d504
Mads M. Pedersen <mmpe@dtu.dk>
2017-07-21 10:20:30 +0200

    updated wetb.utils.test_files.py

--- wetb/utils/test_files.py
+++ wetb/utils/test_files.py
@@ -5,11 +5,8 @@ Created on 20. jul. 2017
 '''
 import os
 import wetb
-import urllib.request
-from fileinput import filename
 import inspect
 wetb_rep_path = os.path.join(os.path.dirname(wetb.__file__), "../")                                   
-default_TestFile_rep_path=os.path.join(os.path.dirname(wetb.__file__) + "/../../TestFiles/")
 
 def get_test_file(filename):
     if not os.path.isabs(filename):
@@ -20,14 +17,12 @@ def get_test_file(filename):
     if os.path.exists(filename):
         return filename
     else:
-        wetb_rep_path = os.path.join(os.path.dirname(wetb.__file__), "../")
         return os.path.join(wetb_rep_path, 'TestFiles', os.path.relpath(filename, wetb_rep_path))
         
 
 
 
-def move2test_files(filename,TestFile_rep_path=default_TestFile_rep_path):
-    wetb_rep_path = os.path.join(os.path.dirname(wetb.__file__), "../")
+def move2test_files(filename):
     dst_filename = os.path.join(wetb_rep_path, 'TestFiles', os.path.relpath(filename, wetb_rep_path))
     folder = os.path.dirname(dst_filename)
     if not os.path.exists(folder):
