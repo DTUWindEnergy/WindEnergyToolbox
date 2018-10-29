20f25230e7e424bd76d3b815c26f9355825390e2
Jennifer Rinker <rink@win.dtu.dk>
2018-10-29 09:37:00 +0100

    updating setup file

--- setup.py
+++ setup.py
@@ -1,45 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-    Setup file for wetb.
+"""Setup file for wetb.
 
-    This file was generated with PyScaffold 2.5, a tool that easily
-    puts up a scaffold for your new Python project. Learn more under:
-    http://pyscaffold.readthedocs.org/
-"""
+Non-dev installation from GitLab: 
+  pip install git+https://gitlab.windenergy.dtu.dk/toolbox/WindEnergyToolbox.git
 
-import os
-import sys
+Non-dev installation from local clone:
+  pip install .
+
+Dev installation from local clone:
+  pip install -e .
+"""
 from setuptools import setup
 
-try:
-    from pypandoc import convert_file
-    read_md = lambda f: convert_file(f, 'rst', format='md')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
-
-import numpy as np
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-
-def setup_package():
-
-    ex_info = [('wetb.fatigue_tools.rainflowcounting', ['pair_range', 'peak_trough', 'rainflowcount_astm']),
-			   ('wetb.signal.filters', ['cy_filters'])]
-    extlist = [Extension('%s.%s' % (module, n),
-                         [os.path.join(module.replace(".","/"), n)+'.pyx'],
-                         include_dirs=[np.get_include()]) for module, names in ex_info for n in names]
-
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
-          cmdclass = {'build_ext': build_ext},
-          ext_modules = extlist,
-          use_pyscaffold=True,
-          long_description=read_md('README.md'))
-
-
-if __name__ == "__main__":
-    setup_package()
+setup(name='Wind Energy Toolbox', 
+      version='2.0a',  
+      description='Wind Energy Toolbox - Common tools for wind energy applications',
+      url='https://gitlab.windenergy.dtu.dk/toolbox/WindEnergyToolbox',
+      author='DTU Wind Energy',  
+      author_email='dave@dtu.dk',
+      license='MIT',
+      packages=['wetb'
+                ],
+      install_requires=[
+        'numpy',  # numerical calculations
+        'pytest',  # testing
+        'pytest-cov',  # calculating coverage
+        'sphinx',  # generating documentation
+        'sphinx_rtd_theme'  # docs theme
+      ],
+      zip_safe=True)
