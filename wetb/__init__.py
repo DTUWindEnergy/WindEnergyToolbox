eb263efefc0b4400e1f59b4c9a1f762753124509
mikf <mikf@dtu.dk>
2019-01-04 14:17:36 +0100

    Update __init__.py

--- wetb/__init__.py
+++ wetb/__init__.py
@@ -5,8 +5,8 @@ from __future__ import absolute_import
 from future import standard_library
 standard_library.install_aliases()
 test = "TEST"
-try:
-    import pkg_resources
-    __version__ = pkg_resources.safe_version(pkg_resources.get_distribution(__name__).version)
-except:
-    __version__ = 'unknown'
+# try:
+#     import pkg_resources
+#     __version__ = pkg_resources.safe_version(pkg_resources.get_distribution(__name__).version)
+# except:
+#     __version__ = 'unknown'
