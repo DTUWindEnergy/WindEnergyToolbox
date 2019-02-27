202596507cc60bc52fb6a57975f92a04b545f4c9
mikf <mikf@dtu.dk>
2019-02-27 12:56:57 +0100

    requirements in setup.py

--- setup.py
+++ setup.py
@@ -25,16 +25,36 @@ from Cython.Distutils import build_ext
 def setup_package():
 
     ex_info = [('wetb.fatigue_tools.rainflowcounting', ['pair_range', 'peak_trough', 'rainflowcount_astm']),
-			   ('wetb.signal.filters', ['cy_filters'])]
+               ('wetb.signal.filters', ['cy_filters'])]
     extlist = [Extension('%s.%s' % (module, n),
-                         [os.path.join(module.replace(".","/"), n)+'.pyx'],
+                         [os.path.join(module.replace(".", "/"), n) + '.pyx'],
                          include_dirs=[np.get_include()]) for module, names in ex_info for n in names]
-
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['six'] + sphinx,
-          cmdclass = {'build_ext': build_ext},
-          ext_modules = extlist,
+    build_requires = ['setuptools_scm',
+                      'future',
+                      'h5py',
+                      'pytables',
+                      'pytest',
+                      'pytest-cov',
+                      'nose',
+                      'blosc',
+                      'pbr',
+                      'paramiko',
+                      'scipy',
+                      'pandas',
+                      'matplotlib',
+                      'cython',
+                      'xlrd',
+                      'coverage',
+                      'xlwt',
+                      'openpyxl',
+                      'psutil',
+                      'six'] + sphinx
+    setup(install_requires=build_requires,
+          setup_requires=build_requires,
+          cmdclass={'build_ext': build_ext},
+          ext_modules=extlist,
           long_description=read_md('README.md'),
           version=__version__,
           packages=find_packages(),
