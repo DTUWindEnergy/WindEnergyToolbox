fad554de1ceea260b9ab5b58e6ed8447f867a5e0
David Verelst <dave@dtu.dk>
2022-11-10 10:14:06 +0100

    wetb.prepost.aep_del: leq was calculated with wrong hour distribution

--- wetb/prepost/aep_del.py
+++ wetb/prepost/aep_del.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Oct 30 13:40:44 2020
@@ -7,20 +6,13 @@ Created on Fri Oct 30 13:40:44 2020
 """
 
 import os
-# from os.path import join as pjoin
-# from glob import glob
 from argparse import ArgumentParser
 import math
 
 import numpy as np
 import pandas as pd
-# from matplotlib import pyplot as plt
 
-# from wetb.prepost import Simulations as sim
 from wetb.dlc.high_level import Weibull_IEC
-# from wetb.prepost.hawcstab2 import results as hs2
-# from wetb.prepost import dlcplots
-# from wetb.prepost.windIO import LoadResults
 
 
 def aep_del(df_stats, resdir):
@@ -31,8 +23,8 @@ def aep_del(df_stats, resdir):
     # prepost-data/{sim_id}_unique-channel-names.csv
     # after running parpost.py
 
-    # by default consider all channels
-    channels = df_stats['channel'].unique()
+    # TODO: make option to have user define list of channels
+    # channels = df_stats['channel'].unique()
 
     # DLC12: assumes that case_id (the file name) has the following format:
     # dlc10_wsp04_wdir000_s1001.htc
@@ -48,8 +40,8 @@ def aep_del(df_stats, resdir):
     # specify which channel to use for the electrical power output
     chpowe = 'DLL-generator_servo-inpvec-2'
     yawdist = {'wdir000':0.5, 'wdir010':0.25, 'wdir350':0.25}
-    dlc12_dir = 'dlc12_iec61400-1ed3'
     dlc10_dir = 'dlc10_powercurve'
+    dlc12_dir = 'dlc12_iec61400-1ed3'
     # -----------------------------------------------------
 
     df_stats['hour_weight'] = 0
@@ -58,7 +50,8 @@ def aep_del(df_stats, resdir):
     # DLC10
     # -----------------
     df_pc_dlc10 = pd.DataFrame()
-    df_dlc10 = df_stats[df_stats['res_dir']==f'{resdir}/{dlc10_dir}']
+    resdir_dlc10 = os.path.join(resdir, dlc10_dir)
+    df_dlc10 = df_stats[df_stats['res_dir']==resdir_dlc10]
     if len(df_dlc10) > 0:
         df_pivot = df_dlc10.pivot(index='case_id', columns='channel', values='mean')
         df_pivot.sort_values(chwind, inplace=True)
@@ -76,7 +69,8 @@ def aep_del(df_stats, resdir):
     # -----------------
     # DLC12
     # -----------------
-    df_dlc12 = df_stats[df_stats['res_dir']==f'{resdir}/{dlc12_dir}'].copy()
+    resdir_dlc12 = os.path.join(resdir, dlc12_dir)
+    df_dlc12 = df_stats[df_stats['res_dir']==resdir_dlc12].copy()
     df_pivot = df_dlc12.pivot(index='case_id', columns='channel', values='mean')
     # now add the wsp, yaw, seed columns again
     tmp = pd.DataFrame(df_pivot.index.values)
@@ -106,7 +100,7 @@ def aep_del(df_stats, resdir):
     # yaw error probabilities
     for yaw, dist in yawdist.items():
         df_pivot.loc[df_pivot['yaw']==yaw, 'hour_weight'] = dist/nr_seeds
-    df_pivot['hours_dist'] = 0
+    df_pivot['annual_hours_wsp'] = 0
 
     # Weibull hour distribution as function of wind speeds
     wsps = df_pivot['wsp'].unique()
@@ -114,17 +108,19 @@ def aep_del(df_stats, resdir):
     wsp_dist = Weibull_IEC(vref, wsps) * fact_1year
     for i, wsp in enumerate(wsps):
         sel = df_pivot['wsp']==wsp
-        df_pivot.loc[sel, 'hours_dist'] = wsp_dist[i]
+        df_pivot.loc[sel, 'annual_hours_wsp'] = wsp_dist[i]
+
+    df_pivot['annual_hour_dist'] = df_pivot['annual_hours_wsp'] * df_pivot['hour_weight']
 
     # check we still have the same amount of hours in a year
-    hours1 = (df_pivot['hours_dist'] * df_pivot['hour_weight']).sum()
+    hours1 = df_pivot['annual_hour_dist'].sum()
     hours2 = wsp_dist.sum()
     assert np.allclose(hours1, hours2)
 
-    aep = (df_pivot['hours_dist'] * df_pivot['hour_weight'] * df_pivot[chpowe]).sum()
+    aep = (df_pivot['annual_hour_dist'] * df_pivot[chpowe]).sum()
 
     df_pc_dlc12 = df_pivot[[chwind, chpowe, 'dlc', 'wsp', 'yaw', 'seed',
-                            'hour_weight', 'hours_dist']].copy()
+                            'hour_weight', 'annual_hours_wsp', 'annual_hour_dist']].copy()
     df_pc_dlc12['aep'] = aep
 
     # -----------------
@@ -133,26 +129,30 @@ def aep_del(df_stats, resdir):
     dict_Leq = {m:[] for m in ms}
     dict_Leq['channel'] = []
 
-    for channel in channels:
+    # TODO: make option to have user define list of channels
+    # for channel in channels:
+        #statsel = df_dlc12[df_dlc12['channel']==channel].copy()
 
-        # sort the m values in the same way as the pivot table is
-        statsel = df_dlc12[df_dlc12['channel']==channel].copy()
+    for channel, grch in df_dlc12.groupby('channel'):
+        # sort the case_id values in the same way as the pivot table is to
+        # align the hours with the corresponding case
+        statsel = grch.copy()
         statsel.set_index('case_id', inplace=True)
-        statsel.sort_index(inplace=True)
+        statsel = statsel.loc[df_pivot.index,:]
 
         dict_Leq['channel'].append(channel)
 
         for m in ms:
             m_ = float(m.split('=')[1])
-
-            R_eq_mod = np.power(statsel[m].values, m_)
+            eq1hz_mod = np.power(statsel[m].values, m_)
             # R_eq_mod will have to be scaled from its simulation length
             # to 1 hour (hour distribution is in hours...). Since the
             # simulation time has not been multiplied out of R_eq_mod yet,
             # we can just multiply with 3600 (instead of doing 3600/neq)
-            tmp = (R_eq_mod * df_pivot['hours_dist'] * years_del * 3600).sum()
+            tmp = (eq1hz_mod * df_pivot['annual_hour_dist'] * years_del * 3600).sum()
             # the effective Leq for each of the material constants
-            dict_Leq[m].append(math.pow(tmp/neq_life, 1.0/m_))
+            leq = math.pow(tmp/neq_life, 1.0/m_)
+            dict_Leq[m].append(leq)
 
     df_leq = pd.DataFrame(dict_Leq)
     df_leq.set_index('channel', inplace=True)
@@ -169,11 +169,11 @@ if __name__ == '__main__':
 
     sim_id = os.path.basename(os.getcwd())
     fname = f'prepost-data/{sim_id}_statistics.h5'
-    wetb_stats = pd.read_hdf(fname, key='table')
+    df_stats = pd.read_hdf(fname, key='table')
 
-    df_pc_dlc10, df_pc_dlc12, df_leq= aep_del(wetb_stats, opt.resdir)
+    df_pc_dlc10, df_pc_dlc12, df_leq= aep_del(df_stats, opt.resdir)
 
     # save fot Excel files
     df_leq.to_excel(f'prepost-data/{sim_id}_del.xlsx')
-    df_pc_dlc10.to_excel(f'prepost-data/{sim_id}_aep.xlsx')
-    df_pc_dlc12.to_excel(f'prepost-data/{sim_id}_aep.xlsx')
+    df_pc_dlc10.to_excel(f'prepost-data/{sim_id}_aep_10.xlsx')
+    df_pc_dlc12.to_excel(f'prepost-data/{sim_id}_aep_12.xlsx')
