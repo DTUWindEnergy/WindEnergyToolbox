d5bb5600d17a9fb88b7a8993d04546a6f7db4fa2
Mads M. Pedersen <mmpe@dtu.dk>
2024-01-17 09:04:16 +0000

    add title to exec_long_task and update ascii2bin

--- wetb/hawc2/ascii2bin/tests/test_ascii2bin.py
+++ wetb/hawc2/ascii2bin/tests/test_ascii2bin.py
@@ -13,22 +13,14 @@ from wetb.hawc2.ascii2bin.ascii2bin import ascii2bin, size_from_file
 testfilepath = os.path.join(os.path.dirname(__file__), 'test_files/')  # test file path
 
 
-class TextUI(object):
-    def show_message(self, m):
-        pass
-
-    def exec_long_task(self, text, allow_cancel, task, *args, **kwargs):
-        return task(*args, **kwargs)
-
 class TestAscii2Bin(unittest.TestCase):
-
 
     def testAscii2bin(self):
 
         for f in ["Hawc2ascii_bin.sel", "Hawc2ascii_bin.dat"]:
             if os.path.exists(testfilepath + f):
                 os.remove(testfilepath + f)
-        ascii2bin(testfilepath + "Hawc2ascii.sel", ui=TextUI())
+        ascii2bin(testfilepath + "Hawc2ascii.sel")
 
         ascii_file = Hawc2io.ReadHawc2(testfilepath + 'Hawc2ascii')
         bin_file = Hawc2io.ReadHawc2(testfilepath + "Hawc2ascii_bin")
@@ -40,7 +32,7 @@ class TestAscii2Bin(unittest.TestCase):
         for f in ["Hawc2bin.sel", "Hawc2bin.dat"]:
             if os.path.exists(testfilepath + f):
                 os.remove(testfilepath + f)
-        ascii2bin(testfilepath + "Hawc2ascii.sel", testfilepath + "Hawc2bin.sel", ui=TextUI())
+        ascii2bin(testfilepath + "Hawc2ascii.sel", testfilepath + "Hawc2bin.sel")
 
         ascii_file = Hawc2io.ReadHawc2(testfilepath + 'Hawc2ascii')
         bin_file = Hawc2io.ReadHawc2(testfilepath + "Hawc2bin")
@@ -48,10 +40,8 @@ class TestAscii2Bin(unittest.TestCase):
         np.testing.assert_array_almost_equal(ascii_file.ReadAscii(), bin_file.ReadBinary(), 1)
         self.assertEqual(ascii_file.ChInfo, bin_file.ChInfo)
 
-
     def testSizeOfFile(self):
         self.assertEqual(size_from_file(testfilepath + "Hawc2ascii.sel"), (800, 28))
-
 
 
 if __name__ == "__main__":
