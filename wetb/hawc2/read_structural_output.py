eb4cddf4e0eb14829600d0e27c78f686bdf45f61
Riccardo Riva <ricriv@dtu.dk>
2024-02-26 15:48:29 +0100

    Added implementation. To be tested

--- wetb/hawc2/read_structural_output.py
+++ wetb/hawc2/read_structural_output.py
@@ -5,23 +5,70 @@ Read HAWC2 results files produced by the `new_htc_structure` commands.
 @author: ricriv
 """
 
+# %% Import.
+
+from glob import glob
+import numpy as np
+
+
+# %% Functions.
+
 def read_body_matrix_output(file_name):
     """
-    Read output of `new_htc_structure` / `body_matrix_output`.
+    Read output of `new_htc_structure` / `body_matrix_output`,
+    which is the mass, damping and stiffness matrices.
 
     Parameters
     ----------
     file_name : str
-        File path.
+        File name entered in the 
+        `new_htc_structure` / `body_matrix_output`
+        command. A glob patter will be used to find all the files.
 
     Returns
     -------
-    mass : ndarray of shape (ndof, ndof)
-        Mass matrix.
-    damping : ndarray of shape (ndof, ndof)
-        Damping matrix.
-    stiffness  : ndarray of shape (ndof, ndof)
-        Stiffness matrix.
-
+    bodies : dict
+        Dictionary containing the structural matrices for each body.
+        Its keys are:
+            mass : ndarray of shape (ndof, ndof)
+                Mass matrix.
+            damping : ndarray of shape (ndof, ndof)
+                Damping matrix.
+            stiffness : ndarray of shape (ndof, ndof)
+                Stiffness matrix.
     """
-    pass
+    # Get all files with the mass matrix.
+    files = glob(f"{file_name}*_m.bin")
+
+    # We get the body names by taking the part between
+    # the file_name and _m.bin.
+    names = [s[len(file_name):-6] for s in files]
+    
+    # Dict that will contain all the results.
+    bodies = dict.fromkeys(names)
+    
+    # Loop over the bodies.
+    for name in names:
+        
+        bodies[name] = {}
+
+        # Read mass matrix.
+        with open(f"{file_name}{name}_m.bin", "r") as fid:
+            ndof = np.fromfile(fid, dtype=int, count=2)[0]
+            bodies[name]["mass"] = np.fromfile(fid, dtype=float, count=ndof*ndof).reshape(ndof, ndof)
+
+        # Read damping matrix.
+        with open(f"{file_name}{name}_c.bin", "r") as fid:
+            ndof = np.fromfile(fid, dtype=int, count=2)[0]
+            bodies[name]["damping"] = np.fromfile(fid, dtype=float, count=ndof*ndof).reshape(ndof, ndof)
+
+        # Read stiffness matrix.
+        with open(f"{file_name}{name}_k.bin", "r") as fid:
+            ndof = np.fromfile(fid, dtype=int, count=2)[0]
+            bodies[name]["stiffness"] = np.fromfile(fid, dtype=float, count=ndof*ndof).reshape(ndof, ndof)
+
+    return bodies
+
+if __name__ == "__main__":
+    
+    bodies = read_body_matrix_output("struct/body_")
