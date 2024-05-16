e6c505c728da1770a56eee706a37a3a27383b3b7
Rasmus Sode Lund <rsod@dtu.dk>
2024-05-16 15:22:39 +0200

    Added a couple more tests, and cleanup

--- wetb/utils/tests/test_installer.py
+++ wetb/utils/tests/test_installer.py
@@ -1,19 +1,46 @@
 import pytest
 
 from wetb.utils.installer import install_wind_tool
+import shutil
 
+DESTINATION="/tmp/hawc2"
 
 def test_installer_zip():
     # Install a program distributed as a zip file
-    install_wind_tool(
-        tool="HAWC2",
-        destination="/tmp/hawc2"
-    )
-
+    try:
+        install_wind_tool(
+            tool="HAWC2",
+            destination=DESTINATION
+        )
+    except:
+        shutil.rmtree(DESTINATION)
+        raise
+    finally:
+        shutil.rmtree(DESTINATION)
 
 def test_installer_executable():
     # Install a program distributed as a standalone executable
-    install_wind_tool(
-        tool="license_manager",
-        destination="/tmp/hawc2"
-    )
+    try:
+        install_wind_tool(
+            tool="license_manager",
+            destination=DESTINATION
+        )
+    except:
+        shutil.rmtree(DESTINATION)
+        raise
+    finally:
+        shutil.rmtree(DESTINATION)
+
+def test_version_not_available():
+    # Test fallback to latest version
+    try:
+        install_wind_tool(
+            tool="HAWC2",
+            version="1.2.3",
+            destination=DESTINATION
+        )
+    except:
+        shutil.rmtree(DESTINATION)
+        raise
+    finally:
+        shutil.rmtree(DESTINATION)
