b3dde80460836aa26feff7a351bbef1833689926
Rasmus Sode Lund <rsod@dtu.dk>
2024-05-16 13:44:11 +0000

    Add chmod+x to installed executables using wetb.utils.installer.install_wind_tool

---
+++ wetb/utils/tests/test_installer.py
@@ -0,0 +1,43 @@
+import pytest
+
+from wetb.utils.installer import install_wind_tool
+import shutil
+
+DESTINATION="/tmp/hawc2"
+
+def test_installer_zip():
+    # Install a program distributed as a zip file
+    try:
+        install_wind_tool(
+            tool="HAWC2",
+            destination=DESTINATION
+        )
+    except:
+        raise
+    finally:
+        shutil.rmtree(DESTINATION)
+
+def test_installer_executable():
+    # Install a program distributed as a standalone executable
+    try:
+        install_wind_tool(
+            tool="license_manager",
+            destination=DESTINATION
+        )
+    except:
+        raise
+    finally:
+        shutil.rmtree(DESTINATION)
+
+def test_version_not_available():
+    # Test fallback to latest version
+    try:
+        install_wind_tool(
+            tool="HAWC2",
+            version="1.2.3",
+            destination=DESTINATION
+        )
+    except:
+        raise
+    finally:
+        shutil.rmtree(DESTINATION)
