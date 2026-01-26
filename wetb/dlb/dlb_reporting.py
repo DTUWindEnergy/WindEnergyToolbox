ee433a529e35dd67fc7686d3f4b02df95146f859
Nicolau Conti Gost <nicgo@dtu.dk>
2026-01-26 15:48:29 +0100

    Rename keyargs, plot names, fix shear force axis labels

--- wetb/dlb/dlb_reporting.py
+++ wetb/dlb/dlb_reporting.py
@@ -55,14 +55,14 @@ def DLB_extreme_loads_to_excel(DLB_extreme_loads, path):
         workbook[sensor].cell(row=15, column=10).fill = highlight
     workbook.save(path)
 
-def plot_DLB_directional_extreme_loads(DLB_extreme_loads, folder, extension='.png'):
+def plot_DLB_directional_extreme_loads(DLB_directional_extreme_loads, folder, extension='.png'):
     """
     Plot the DLB directional extreme loads and save them to a given folder.
 
     Parameters
     ----------
-    DLB_extreme_loads : xarray.DataArray ('sensor_name', 'angle')
-        DataArray containing the extreme loads of the DLB.
+    DLB_directional_extreme_loads : xarray.DataArray ('sensor_name', 'angle')
+        DataArray containing the directional extreme loads of the DLB.
     folder : str
         Path to the folder where the plots will be saved
     extension: str, optional
@@ -73,36 +73,41 @@ def plot_DLB_directional_extreme_loads(DLB_extreme_loads, folder, extension='.pn
     None.
 
     """
-    angles = DLB_extreme_loads.coords['angle'].values
-    for i in range(DLB_extreme_loads.shape[0]):
-        x = [float(DLB_extreme_loads[i, j])*np.cos(np.deg2rad(angles[j]))
+    os.makedirs(folder, exist_ok=True)
+    angles = DLB_directional_extreme_loads.coords['angle'].values
+    units = DLB_directional_extreme_loads.coords['sensor_unit'].values
+    for i in range(DLB_directional_extreme_loads.shape[0]):
+        x = [float(DLB_directional_extreme_loads[i, j])*np.cos(np.deg2rad(angles[j]))
              for j in range(len(angles))]
-        y = [float(DLB_extreme_loads[i, j])*np.sin(np.deg2rad(angles[j]))
+        y = [float(DLB_directional_extreme_loads[i, j])*np.sin(np.deg2rad(angles[j]))
              for j in range(len(angles))]
-        DLC = [get_DLC(DLB_extreme_loads[i, j].group.values[()]) for j in range(len(angles))]
+        DLC = [get_DLC(DLB_directional_extreme_loads[i, j].group.values[()]) for j in range(len(angles))]
         plt.scatter(x, y)
         [plt.plot([0, x[k]], [0, y[k]], color='black') for k in range(len(x))]
-        plt.xlabel('Mx')
-        plt.ylabel('My')
-        plt.title(DLB_extreme_loads[i].coords['sensor_name'].values[()])
+        if units[i] == 'kN':
+            plt.xlabel('Fx')
+            plt.ylabel('Fy')
+        if units[i] == 'kNm':
+            plt.xlabel('Mx')
+            plt.ylabel('My')
+        plt.title(DLB_directional_extreme_loads[i].coords['sensor_name'].values[()])
         plt.axhline(0, color='black',linewidth=1)  
         plt.axvline(0, color='black',linewidth=1)  
         plt.xlim(-max(abs(min(x)), abs(max(x)))*1.2, max(abs(min(x)), abs(max(x)))*1.2)
         plt.ylim(-max(abs(min(y)), abs(max(y)))*1.2, max(abs(min(y)), abs(max(y)))*1.2)
         for j in range(len(x)):
             plt.annotate(DLC[j], (x[j], y[j]), textcoords="offset points", xytext=(0,10), ha='center')
-        plt.savefig(os.path.join(folder, 'Extreme_' + DLB_extreme_loads[i].coords['sensor_name'].values[()] + extension))
+        plt.savefig(os.path.join(folder, DLB_directional_extreme_loads[i].coords['sensor_name'].values[()] + '_directional_extreme_load' + extension))
         plt.show()
         
-def plot_DLB_directional_equivalent_loads(DLB_fatigue_loads, folder, extension='.png'):
+def plot_DLB_directional_equivalent_loads(DLB_directional_eq_loads, folder, extension='.png'):
     """
     Plot the DLB directional equivalent loads and save them to a given folder.
 
     Parameters
     ----------
-    DLB_fatigue_loads : xarray.DataArray ('sensor_name', 'angle', 'm')
-        DataArray containing the fatigue loads of the DLB. It matches the
-        output from get_DLB_fatigue_loads
+    DLB_directional_eq_loads : xarray.DataArray ('sensor_name', 'angle', 'm')
+        DataArray containing the directional equivalent loads of the DLB.
     folder : str
         Path to the folder where the plots will be saved
     extension: str, optional
@@ -113,23 +118,29 @@ def plot_DLB_directional_equivalent_loads(DLB_fatigue_loads, folder, extension='
     None.
 
     """
-    m_list = DLB_fatigue_loads.coords['m'].values
-    angles = DLB_fatigue_loads.coords['angle'].values
-    for i in range(DLB_fatigue_loads.shape[0]):
+    os.makedirs(folder, exist_ok=True)
+    m_list = DLB_directional_eq_loads.coords['m'].values
+    angles = DLB_directional_eq_loads.coords['angle'].values
+    units = DLB_directional_eq_loads.coords['sensor_unit'].values
+    for i in range(DLB_directional_eq_loads.shape[0]):
         for j in range(len(m_list)):
-            x = [float(DLB_fatigue_loads[i, k, j])*np.cos(np.deg2rad(angles[k]))
+            x = [float(DLB_directional_eq_loads[i, k, j])*np.cos(np.deg2rad(angles[k]))
                  for k in range(len(angles))]
-            y = [float(DLB_fatigue_loads[i, k, j])*np.sin(np.deg2rad(angles[k]))
+            y = [float(DLB_directional_eq_loads[i, k, j])*np.sin(np.deg2rad(angles[k]))
                  for k in range(len(angles))]
             plt.scatter(x, y, label='m = ' + str(m_list[j]))
             [plt.plot([0, x[k]], [0, y[k]], color='black') for k in range(len(angles))]
-        plt.xlabel('Mx')
-        plt.ylabel('My')
-        plt.title(DLB_fatigue_loads[i].coords['sensor_name'].values[()])
+        if units[i] == 'kN':
+            plt.xlabel('Fx')
+            plt.ylabel('Fy')
+        if units[i] == 'kNm':
+            plt.xlabel('Mx')
+            plt.ylabel('My')
+        plt.title(DLB_directional_eq_loads[i].coords['sensor_name'].values[()])
         plt.axhline(0, color='black',linewidth=1)
         plt.axvline(0, color='black',linewidth=1)
         plt.xlim(-max(abs(min(x)), abs(max(x)))*1.2, max(abs(min(x)), abs(max(x)))*1.2)
         plt.ylim(-max(abs(min(y)), abs(max(y)))*1.2, max(abs(min(y)), abs(max(y)))*1.2)
         plt.legend()
-        plt.savefig(os.path.join(folder, 'Fatigue_' + DLB_fatigue_loads[i].coords['sensor_name'].values[()] + extension))
+        plt.savefig(os.path.join(folder, DLB_directional_eq_loads[i].coords['sensor_name'].values[()] + '_directional_equivalent_load' + extension))
         plt.show()
