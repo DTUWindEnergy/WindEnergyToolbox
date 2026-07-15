31ff7a4f3bd0ba1a4ac32d4a67075942ebdbd973
Mads M. Pedersen <mmpe@dtu.dk>
2026-07-15 15:04:37 +0200

    all

---
+++ wetb/__init__.py
@@ -0,0 +1,7 @@
+test = "TEST"
+__version__ = '0.0.10'
+# try:
+#     import pkg_resources
+#     __version__ = pkg_resources.safe_version(pkg_resources.get_distribution(__name__).version)
+# except:
+#     __version__ = 'unknown'
