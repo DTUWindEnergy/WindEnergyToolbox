6509bd327b362b31ec3e25ecbca1ef73cd3bb162
Nicolau Conti Gost <nicgo@dtu.dk>
2026-07-17 11:33:42 +0200

    get_Mann_params stores Nxyz, dxyz and double_xyz as tuples

--- wetb/dlb/iec61400_1.py
+++ wetb/dlb/iec61400_1.py
@@ -413,9 +413,9 @@ class DLB():
         for i in range(len(sims)):
             if sims['seed'].iloc[i] is not None and not np.isnan(sims['seed'].iloc[i]):
                 dx = sims['V_hub'].iloc[i] * sims['simulation_time'].iloc[i] / (Nxyz[0] - 1)
-                if (L, Nxyz[0], Nxyz[1], Nxyz[2], dx, dy, dz, sims['seed'].iloc[i]) not in Mann_params:
-                    Mann_params.append((L, Nxyz[0], Nxyz[1], Nxyz[2], dx, dy, dz, sims['seed'].iloc[i]))
-        return pd.DataFrame(Mann_params, columns=['L', 'Nx', 'Ny', 'Nz', 'dx', 'dy', 'dz', 'seed'])
+                if (L, Nxyz, (dx, dy, dz), sims['seed'].iloc[i]) not in Mann_params:
+                    Mann_params.append((L, Nxyz, (dx, dy, dz), sims['seed'].iloc[i]))
+        return pd.DataFrame(Mann_params, columns=['L', 'Nxyz', 'dxyz', 'seed'])
 
 
 class DTU_IEC61400_1_Ref_DLB(DLB):
